b16a282e43ca5685a9bf0818778f651579fad934
Rexford <seanmavley@gmail.com>
2016-04-27 21:16:23 +0000

    load view 3 custom data successfully

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
     $('.dropdown-button').dropdown({
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: false // Displays dropdown below the button
-    }
-  );
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false // Displays dropdown below the button
+    });
+
+    $('.search-toggle').click(function() {
+        if ($('.hiddensearch').css('display') == 'none')
+            $('.hiddensearch').slideDown();
+        else
+            $('.hiddensearch').slideUp();
+    });
 });
