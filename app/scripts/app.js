c83df727a069bed4550beb6add7e66dde2026dfa
Rexford <seanmavley@gmail.com>
2016-11-08 12:01:01 +0000

    enabled clean urls via server urls rewriting

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,25 @@
 angular.module('corp', ['ngRoute', 'appController', 'FileService', 'ng-fusioncharts'])
 
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+
     $routeProvider
-        .when('/', {
-            templateUrl: 'templates/home.html',
-            controller: 'view1'
-        })
-        .when('/view2', {
-            templateUrl: 'templates/view2.html',
-            controller: 'view2'
-        })
-        .when('/view3', {
-            templateUrl: 'templates/view3.html',
-            controller: 'view3'
-        })
-        .otherwise({
-            redirectTo: '/'
-        })
-}])
+      .when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'view1'
+      })
+      .when('/view2', {
+        templateUrl: 'templates/view2.html',
+        controller: 'view2'
+      })
+      .when('/view3', {
+        templateUrl: 'templates/view3.html',
+        controller: 'view3'
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
+      $locationProvider.html5Mode(true);
+  }
+])
--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,36 @@ var gulp = require('gulp'),
   $ = require('gulp-load-plugins')(),
   concat = require('gulp-concat'),
   rename = require('gulp-rename'),
-  uglify = require('gulp-uglify');
+  uglify = require('gulp-uglify'),
+  historyApiFallback = require('connect-history-api-fallback');
 
 var port = process.env.SERVER_PORT || 3000;
 
 //script paths
-var jsFiles = 'app/scripts/**/*.js',  
-    jsDest = 'app/build';
+var jsFiles = 'app/scripts/**/*.js',
+  jsDest = 'app/build';
 
-gulp.task('scripts', function() {  
-    return gulp.src(jsFiles)
-        .pipe(concat('corp.darshboard.js'))
-        .pipe(gulp.dest(jsDest))
-        .pipe(rename('corp.dashboard.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(jsDest));
+// build javascripts
+gulp.task('scripts', function() {
+  return gulp.src(jsFiles)
+    .pipe(concat('corp.darshboard.js'))
+    .pipe(gulp.dest(jsDest))
+    .pipe(rename('corp.dashboard.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDest));
 });
 
+// server and run in browser
 gulp.task('serve', function() {
-    browser.init({ server: 'app/', port: port });
-    // watch and rebuild scripts
-    gulp.watch('app/**/*.*', ['scripts'])
-      .on('change', browser.reload);
+  browser.init({
+    server: 'app/',
+    port: port,
+    // rewrites urls to support clean URLs without the '#/'s
+    middleware: [historyApiFallback()] 
+  });
+  // watch and rebuild scripts
+  gulp.watch('app/**/*.*', ['scripts'])
+    .on('change', browser.reload);
 });
 
 gulp.task('default', ['serve']);
